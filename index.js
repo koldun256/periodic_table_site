088acdaa3ea88ed955161376c8494786fee5b1d4
koldun256 <eg.sharypov@ya.ru>
2019-10-20 23:35:34 +0700

    elements and menues pages

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const app = express();
 const elements_data = require('./elements_data');
 const http = require('http').createServer(app);
 const PORT = process.env.PORT || 5000;
+const MENU = '/';
 app.use(express.static('public'));
 app.set('view engine','ejs');
-app.get('/element/:id',(request, response)=>{
-    response.render('element',elements_data[parseInt(request.params['id'])]);
+app.get(MENU,(req,res)=>{
+    console.log(req.headers.host);
+    res.render('menu',{elements: elements_data,element_url: 'http://'+req.headers.host+'/element/'});
+});
+app.get('/element/:id',(req, res)=>{
+    res.render('element',{element: elements_data[parseInt(req.params['id'])],
+                        menuLocation: 'http://'+req.headers.host+MENU});
 });
 http.listen(PORT,()=>console.log(`listening on port ${PORT}`));
